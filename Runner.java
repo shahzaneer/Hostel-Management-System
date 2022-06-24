1159c8a8bc32738e5867bfdf1bba9e5b32d7837e
Shahzaneer Ahmed <shahzaneer.dev@gmail.com>
2022-06-24 15:56:53 +0500

    Project Completed (Evaluated) ✅

--- Runner.java
+++ Runner.java
@@ -3,5 +3,7 @@ import UIScreen.*;
 public class Runner {
     public static void main(String[] DataScientist) {
         new Main();
+
+        // gahay khandam , gahay giryam , gahay khezam ghahey uftam maseeha dar dilam paida wa man bemar mi gardam! - RUMI ❤
     }                                                                  
 }
